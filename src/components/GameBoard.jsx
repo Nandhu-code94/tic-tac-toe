ba44294850b5c55545e4211d912ba6ee7498c275
Nandhu-code94 <Nandhu28041994@gmail.com>
2025-06-22 17:36:11 +0530

    tic

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,26 @@
+
+export default function GameBoard({onSelectSquare, board}) {
+
+    // const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    //     function handleSelectSquare (rowIndex, columnIndex) {
+    //         setGameBoard((prevGameBoard)=>{
+    //             const updatedBoard = [...prevGameBoard.map((innerArray)=> [...innerArray])]
+    //             updatedBoard[rowIndex][columnIndex] = activePlayerSymbol;       
+    //             return updatedBoard
+    //         });
+    //         onSelectSquare();
+    //     }
+    return (
+        <ol id="game-board">
+            {board?.map((row, rowIndex)=><li key={rowIndex}>
+                <ol>
+                    {row.map((playerSymbol, colIndex)=> <li key={colIndex}>
+                        <button onClick={() => onSelectSquare(rowIndex, colIndex)}
+                        disabled={playerSymbol !== null}
+                            >{playerSymbol}</button></li>)}
+                </ol>
+            </li>)}
+        </ol>
+    );
+};
